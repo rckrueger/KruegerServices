f7acf6363760f39172bcc57d16679cacdf15876e
rckrueger <krueger@skinux.com>
2021-05-05 14:50:48 -0400

    first commit

--- Sources/KruegerServices/KruegerServices.swift
+++ Sources/KruegerServices/KruegerServices.swift
@@ -1,3 +1,14 @@
-struct KruegerServices {
-    var text = "Hello, World!"
+//
+//  KruegerServices.swift
+//  toto
+//
+//  Created by Richard Krueger on 5/5/21.
+//
+
+import Foundation
+
+public struct KruegerServices {
+    public static func doSomething() {
+        print("Did something man")
+    }
 }
